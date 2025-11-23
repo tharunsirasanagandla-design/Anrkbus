6c28165ab87c0144a57feee1bb8f41fe546e8228
tharunsirasanagandla-design <tharunsirasanagandla@gmail.com>
2025-11-23 11:25:04 +0530

    Refactor map initialization and bus marker logic

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-// Set bus icon
-var busIcon = L.icon({
-    iconUrl: 'https://cdn-icons-png.flaticon.com/512/61/61212.png', 
-    iconSize: [40, 40],
-    iconAnchor: [20, 20]
+document.addEventListener("DOMContentLoaded", function () {
+    var map = L.map('map').setView([17.1466, 79.6221], 10);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19
+    }).addTo(map);
+
+    // BUS ICON
+    var busIcon = L.divIcon({
+        html: "🚌",  
+        className: "bus-icon",
+        iconSize: [40, 40],
+        iconAnchor: [20, 20]
+    });
+
+    var marker = L.marker([17.1466, 79.6221], { icon: busIcon }).addTo(map);
+
+    function updateLocation() {
+        fetch('location.json')
+            .then(response => response.json())
+            .then(data => {
+                marker.setLatLng([data.latitude, data.longitude]);
+                map.setView([data.latitude, data.longitude], 13);
+            });
+    }
+
+    updateLocation();
+    setInterval(updateLocation, 5000);
 });
-
-// Create map
-var map = L.map('map').setView([16.5925, 79.0075], 11);
-
-// Load & display tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(map);
-
-// Add marker (bus)
-var marker = L.marker([16.5925, 79.0075], { icon: busIcon }).addTo(map);
-
-// Update location (static example)
-function updateLocation() {
-    var newLat = 16.5925 + (Math.random() - 0.5) * 0.01;
-    var newLng = 79.0075 + (Math.random() - 0.5) * 0.01;
-
-    marker.setLatLng([newLat, newLng]);
-    map.setView([newLat, newLng]);
-}
-
-// Update every 5 seconds
-setInterval(updateLocation, 5000);
